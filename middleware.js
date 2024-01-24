bf49eada3653a9122da6b3a6d11a2c0ee6dccc98
victuseze <106381679+victuseze@users.noreply.github.com>
2024-01-24 21:55:33 +0000

    Middleware giving issues

--- middleware.js
+++ middleware.js
@@ -1,23 +1,10 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from "next/server"
 
-export function middleware(request) {
-    console.log('Middleware Working')
-    return NextResponse.json({result: 'Middle-ware Being Successful!'})
+export function middleware(request){
+    console.log({request})
+    return NextResponse.json({success: "Middleware Is Successful!"})
 }
-
-// Now we have to specify where we want our middleware to redirect us to.
 
 export const config = {
-    matcher: ["/userslist/user/:path*"]
-}
-
-// import { NextResponse } from "next/server";
-
-// export function middleware(req, res) {
-//   console.log('Middleware Working');
-//   return NextResponse.json({ result: 'Middle-ware Being Successful!' });
-// }
-
-// export const config = {
-//   matcher: '/mid/:path*',
-// };
+    matcher: '/userslist/:path*',
+  }
--- src/page.js
+++ src/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page Going</div>
+    <div>Landing Page</div>
   )
 }
 
